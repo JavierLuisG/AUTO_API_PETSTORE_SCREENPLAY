50cd64f01e5d861804d3ea61cfb5ad01a0d36958
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2026-03-13 14:37:41 -0500

    feat: add TestRunnerUser class for user-related test execution

---
+++ src/test/java/com/sofkianos/runners/TestRunnerUser.java
@@ -0,0 +1,15 @@
+package com.sofkianos.runners;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"com.sofkianos.stepdefinitions", "com.sofkianos.hooks"},
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        plugin = {"pretty"}
+)
+public class TestRunnerUser {
+}
